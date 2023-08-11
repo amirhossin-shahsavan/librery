95376b475b83004003fa2f9b4815b7eecc05217d
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-08-11 10:56:56 +0330

    create books model.

---
+++ models/Book.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  releaseDate: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category'
+  },
+  pageCount: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  minage: {
+    type: String,
+    required: true
+  },
+  active: {
+    type: String,
+    required: true
+  },
+  tags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag'
+  }],
+  isAccepted: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
