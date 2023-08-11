f1887cb6b2d2b8305266266ee3405a3262d44797
mohamamdreza Taherian <taherian.1993@gmail.com>
2023-08-11 12:54:32 +0330

    added method login for users

--- controllers/auth/userAuth.js
+++ controllers/auth/userAuth.js
@@ -1,14 +1,28 @@
 const People = require('../../models/users')
 
-exports.signUp = (req,res)=>{
-   const  {name, family, role, age, cellPhone, brithDay, active} = req.body
+exports.login = (req,res)=>{
+   const  {cellPhone, password } = req.body
 
-    console.log(`>>>>${name, family, role, age, cellPhone, brithDay, active}`)
-    const poeple = new People({
-        name, family, role, age, cellPhone, brithDay, active
-    })
-    poeple.save()
-    .then(savedUser => res.status(201).json(savedUser))
-    .catch(err => res.status(500).json({ error: err.message }));
+    console.log(`>>>>${ cellPhone,password }`)
+        People.findOne({
+            cellPhone,
+        }).then((user)=>{
+            if(!user){
+                console.log("user not found")
+                res.send("user not found")
+            }
+            else 
+            res.send("user is login")
+        })
     };
-
+    exports.signUp = (req,res)=>{
+        const  {name, family, role, age, cellPhone, brithDay, active} = req.body
+     
+         console.log(`>>>>${name, family, role, age, cellPhone, brithDay, active}`)
+         const poeple = new People({
+             name, family, role, age, cellPhone, brithDay, active
+         })
+         poeple.save()
+         .then(savedUser => res.status(201).json(savedUser))
+         .catch(err => res.status(500).json({ error: err.message }));
+         };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const userAuthController = require("../controllers/auth/userAuth")
 
 router.post("/signup", userAuthController.signUp)
+router.post("/login", userAuthController.login)
 
 
 module.exports = router
